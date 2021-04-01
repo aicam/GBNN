5de509cb21859d32ddefa851e0d6a623bdaa48df
ali <a_icam@yahoo.com>
2021-04-01 17:12:01 +0430

    get partial charges completed

--- Featurizer/get_charges.py
+++ Featurizer/get_charges.py
@@ -1,15 +1,20 @@
 import subprocess
 import os
+from biopandas.pdb import PandasPdb
 
 
 
 
-os.chdir("get_charges")
-result = subprocess.Popen(["./get_charges.sh ~/calstate/amber/ras.pdb"], executable="/bin/bash",
-                          shell=True,
-                          stdout= subprocess.PIPE,
-                          stderr= subprocess.PIPE)
-# result = subprocess.run(['cd get_charges', ''], stdout=subprocess.PIPE)
-s, e = result.communicate()
-print(s.decode('utf-8'))
-print(e)
+def get_pdb_charges(pdbfile):
+    os.chdir("get_charges")
+    result = subprocess.Popen(["./get_charges.sh " + pdbfile], executable="/bin/bash",
+                              shell=True,
+                              stdout= subprocess.PIPE,
+                              stderr= subprocess.PIPE)
+    s, e = result.communicate()
+    pandaspdb = PandasPdb()
+    # print(s.decode("utf-8"))
+    ppdb_df = pandaspdb.read_pdb_from_list(pdb_lines=s.decode("utf-8").split("\n"))
+    return ppdb_df.df['ATOM']
+
+print(get_pdb_charges("~/calstate/amber/ras.pdb")["b_factor"])
