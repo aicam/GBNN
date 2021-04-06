cedcd31aa60993840783fdb98f9f50c440f7ff1f
ali <a_icam@yahoo.com>
2021-04-06 11:16:36 +0430

    model editting

--- GBModel/GB_Layers.py
+++ GBModel/GB_Layers.py
@@ -4,15 +4,22 @@ from tensorflow import keras
 
 class MMLayer(keras.layers.Layer):
 
-    def __init__(self, input_dim):
+    def __init__(self):
         super(MMLayer, self).__init__()
         self.alpha = tf.Variable(initial_value=tf.ones(1), trainable=True)
-        self.multiplied_ew = tf.Variable(initial_value=tf.zeros(input_dim), trainable=False)
+        # self.multiplied_ew = tf.Variable(initial_value=tf.zeros(input_dim), trainable=False)
+
+    def cell_function_map(self, input):
+        multiplied_ew = (tf.multiply(input[:, :, :1], input[:, :, 1:2]))
+        return tf.concat([tf.multiply(tf.exp(multiplied_ew), self.alpha),
+                          tf.reshape(input[:, :, 2], [tf.shape(input)[0], tf.shape(input)[1], 1])], axis=2)
+
 
     def call(self, inputs):
-        self.multiplied_ew.assign(tf.multiply(inputs[0,:, :, :1], inputs[0,:, :, 1:2]))
-        return tf.concat([tf.multiply(tf.exp(self.multiplied_ew), self.alpha),
-                          tf.reshape(inputs[0, :, :, 2], [tf.shape(inputs)[1], tf.shape(inputs)[2], 1])], axis=2)
+        return tf.map_fn(fn = lambda i: self.cell_function_map(i), elems=inputs)
+        # self.multiplied_ew.assign(tf.multiply(inputs[0,:, :, :1], inputs[0,:, :, 1:2]))
+        # return tf.concat([tf.multiply(tf.exp(self.multiplied_ew), self.alpha),
+        #                   tf.reshape(inputs[0, :, :, 2], [tf.shape(inputs)[1], tf.shape(inputs)[2], 1])], axis=2)
 
 
 class GBLayer(keras.layers.Layer):
@@ -23,11 +30,14 @@ class GBLayer(keras.layers.Layer):
         self.beta = tf.Variable(initial_value=tf.zeros(1), trainable=True)
         self.gamma = tf.Variable(initial_value=tf.ones(1), trainable=True)
 
-    def call(self, inputs):
-        m_matrix = inputs[:, :, 0]
-        q_matrix = inputs[:, :, 1]
+    def cell_function_map(self, input):
+        m_matrix = input[:, :, 0]
+        q_matrix = input[:, :, 1]
         return tf.multiply(tf.multiply(1 / m_matrix, tf.multiply(q_matrix, self.alpha)),
                            tf.exp((tf.math.pow(m_matrix, 2) - tf.math.pow(self.beta, 2)) / tf.math.pow(self.gamma, 2)))
+
+    def call(self, inputs):
+        return tf.map_fn(fn = lambda i: self.cell_function_map(i), elems = inputs)
 
 
 class FilterLayer(keras.layers.Layer):
@@ -36,5 +46,9 @@ class FilterLayer(keras.layers.Layer):
         super(FilterLayer, self).__init__()
         self.w = tf.Variable(initial_value=tf.random.normal(shape=input_dim), trainable=True)
 
+    def cell_function_map(self, input):
+        return tf.reshape(tf.matmul(input, self.w), [input.shape[0], self.w.shape[1], 1])
+
     def call(self, inputs):
-        return tf.reshape(tf.matmul(inputs, self.w), [1, inputs.shape[0], self.w.shape[1], 1])
+        return tf.map_fn(fn = lambda i: self.cell_function_map(i), elems = inputs)
+
--- main.py
+++ main.py
@@ -4,14 +4,18 @@ import numpy as np
 
 x = np.ones((1, 8, 4, 3))
 from deepchem import models
-
+# x1 = MMLayer()(x)
+# print(x1.shape)
+# x2 = GBLayer()(x1)
+# print(x2.shape)
+# print(FilterLayer([4, 10])(x2).shape)
 model = tf.keras.models.Sequential()
-model.add(MMLayer([8, 4, 1]))
+model.add(MMLayer())
 model.add(GBLayer())
 model.add(FilterLayer([4, 10]))
 model.add(tf.keras.layers.Conv2D(4, (2, 2)))
 model.add(tf.keras.layers.Flatten())
 model.add(tf.keras.layers.Dense(1, activation='relu'))
 model.compile(loss='MSE', optimizer='adam')
-# print(model(x))
-print(model.fit(x, np.zeros([1, 1]), batch_size=1, epochs=2))
+
+print(model.fit(x, np.zeros([1, 1]), epochs=2))
