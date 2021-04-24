961870698abf26385fac0fc9b824250007e69bde
ali <a_icam@yahoo.com>
2021-04-24 12:54:17 +0430

    download pdb script finished

---
+++ scripts/PDB_downloader.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import numpy as np
+import os
+import sys
+import urllib
+
+PDB_directory = '../PDBs'
+
+
+def download_pdb(pdbcode, datadir, downloadurl="https://files.rcsb.org/download/"):
+    """
+    Downloads a PDB file from the Internet and saves it in a data directory.
+    :param pdbcode: The standard PDB ID e.g. '3ICB' or '3icb'
+    :param datadir: The directory where the downloaded file will be saved
+    :param downloadurl: The base PDB download URL, cf.
+        `https://www.rcsb.org/pages/download/http#structures` for details
+    :return: the full path to the downloaded PDB file or None if something went wrong
+    """
+    pdbfn = pdbcode + ".pdb"
+    url = downloadurl + pdbfn
+    outfnm = os.path.join(datadir, pdbfn)
+    try:
+        urllib.request.urlretrieve(url, outfnm)
+        return outfnm
+    except Exception as err:
+        print(str(err), file=sys.stderr)
+        return None
+
+df = pd.read_csv('re1.csv')
+
+for i in range(len(df)):
+    print("PDB " + df['PDB code'][i] + " started")
+    download_pdb(df['PDB code'][i], PDB_directory)
