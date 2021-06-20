e84c985087c1829d1fe3d32c530cb3b6fdc18a66
ali <a_icam@yahoo.com>
2021-06-20 19:10:34 +0430

    new featurizer for moobley dataset

--- Featurizer/gbnsr6/gbnsr6.sh
+++ Featurizer/gbnsr6/gbnsr6.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cp $1 ./guest-1.mol2
-cp $2 ./
+cp $2 ./host-cb7.mol2
 
 
 
@@ -9,3 +9,5 @@ $AMBERHOME/bin/parmchk2 -i g1.prep -f prepi -o g1.frcmod
 
 $AMBERHOME/bin/tleap -f tleap_gbnsr6.in
 $AMBERHOME/bin/gbnsr6 -O -i gbnsr6.in -p cb7-solvated.prmtop -c cb7-solvated.inpcrd -o cb7-gbout
+$AMBERHOME/bin/gbnsr6 -O -i gbnsr6.in -p g1-solvated.prmtop -c g1-solvated.inpcrd -o g1-gbout
+$AMBERHOME/bin/gbnsr6 -O -i gbnsr6.in -p cb7-g1-solvated.prmtop -c cb7-g1-solvated.inpcrd -o cb7-g1-gbout
