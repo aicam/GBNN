7adf8ffaa3c85f4e5c087fb421f8793c7900a1ba
ali <a_icam@yahoo.com>
2021-09-29 13:07:55 +0330

    moobley featurizer

---
+++ ConvFeaturizeModel/main.py
@@ -0,0 +1,16 @@
+from GBModel.GB_Layers import MMLayer, GBLayer, FilterLayer
+import tensorflow as tf
+from deepchem.models.layers import GraphConv, GraphPool, GraphGather
+
+import numpy as np
+from deepchem.feat.mol_graphs import ConvMol
+x = np.ones((1, 8, 4, 3))
+model = tf.keras.models.Sequential()
+model.add(MMLayer())
+model.add(GBLayer())
+model.add(FilterLayer([4, 10]))
+model.add(tf.keras.layers.Conv2D(4, (2, 2)))
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(1, activation='relu'))
+model.compile(loss='MSE', optimizer='adam')
+
