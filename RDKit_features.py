fe866909f0c83ccb235bea267bc0113cea57377f
ali <a_icam@yahoo.com>
2021-10-19 12:55:07 +0330

    new model with variable train test split finished

---
+++ RDKit_features.py
@@ -0,0 +1,10 @@
+# !curl -Lo conda_installer.py https://raw.githubusercontent.com/deepchem/deepchem/master/scripts/colab_install.py
+import conda_installer
+# conda_installer.install()
+conda_installer.install()
+# !/root/miniconda/bin/conda info -e
+# import rdkit
+import deepchem as dc
+
+# df = pd.read_csv('dataset.csv')
+# df = df.dropna()
