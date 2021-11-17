48ab8c35ad0adfe238c050672b5daa3ee6538010
ali <a_icam@yahoo.com>
2021-11-17 16:59:13 +0330

    PGGCN featurizer started

---
+++ PGGCN/Featurizer.py
@@ -0,0 +1,11 @@
+import numpy as np
+from deepchem.feat.graph_features import ConvMolFeaturizer
+
+class PGGCNFeaturizer(ConvMolFeaturizer):
+
+
+
+    def __init__(self,
+
+                 ):
+        super(PGGCNFeaturizer, self).__init__()
