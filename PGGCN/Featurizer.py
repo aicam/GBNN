bb4df379ab9cceb4f2197b49a9e190dd0fb68d44
ali <a_icam@yahoo.com>
2021-11-18 21:04:39 +0330

    PGGCN featurizer

--- PGGCN/Featurizer.py
+++ PGGCN/Featurizer.py
@@ -1,14 +1,18 @@
 import numpy as np
 import deepchem as dc
 import os
+import pandas as pd
 
 class PGGCNFeaturizer:
 
 
 
-    def __init__(self, PDB_directory, physics_csv, atom_csv_directory):
-        self.PDB_directory = PDB_directory
-        self.physics_csv = physics_csv
-        self.atom_csv_directory = atom_csv_directory
+    def __init__(self, mols_dict, physics_csv, atom_csvs_directory):
+        self.mols_dict = mols_dict
+        self.physics_df = pd.read_csv(physics_csv)
+        self.atoms_csvs_directory = atom_csvs_directory if atom_csvs_directory[-1] == '/' else atom_csvs_directory + '/'
 
     def featurize(self):
+        atoms_csvs = [self.atoms_csvs_directory + atoms_csv for atoms_csv in os.listdir(self.atoms_csvs_directory) if atoms_csv.__contains__('.csv')]
+        for mol_name in self.mols_dict.keys():
+
