91c811bcc34db79f1034a3769fbb0b7ecb36add1
ali <a_icam@yahoo.com>
2021-11-19 00:37:01 +0330

    PGGCN featurizer

--- PGGCN/Featurizer.py
+++ PGGCN/Featurizer.py
@@ -2,11 +2,10 @@ import numpy as np
 import deepchem as dc
 import os
 import pandas as pd
+import rdkit
+from rdkit import Chem
 
 class PGGCNFeaturizer:
-
-
-
     def __init__(self, mols_dict, physics_csv, atom_csvs_directory):
         self.mols_dict = mols_dict
         self.physics_df = pd.read_csv(physics_csv)
@@ -16,7 +15,8 @@ class PGGCNFeaturizer:
         self.featurizer = dc.feat.ConvMolFeaturizer(per_atom_fragmentation=False)
 
     def featurize(self):
-
+        # input array will returned to use in model as input
+        inputs = []
         # Find all atoms GBNSR6 features csv files
         atoms_csvs = [self.atoms_csvs_directory + atoms_csv for atoms_csv in os.listdir(self.atoms_csvs_directory) if atoms_csv.__contains__('.csv')]
 
@@ -24,11 +24,15 @@ class PGGCNFeaturizer:
         atoms_csvs_dict = {atoms_csv.split('/')[-1].split('.')[0].split('-')[0]: pd.read_csv(atoms_csv).round(3) for atoms_csv in
                            atoms_csvs}
 
-
+        # featurizing each complex by mols_dict
         for mol_name, mol in self.mols_dict.items():
+            # molecule featurized
             mol_f = self.featurizer.featurize(mol)[0]
+            # atoms features extracted by Amber
             atoms_gb = atoms_csvs_dict[mol_name]
-
+            # hydrogens are used by Amber but RDKit remove them so Amber features should be re ordered
+            # atom positions are unique and can be used to equalize orders
+            rdkit_order = None
             for c in mol.GetConformers():
                 rdkit_order = c.GetPositions()
             df = pd.DataFrame(columns=atoms_gb.columns)
@@ -36,7 +40,34 @@ class PGGCNFeaturizer:
                 df = df.append(atoms_gb[(atoms_gb['x'] == atom_pos[0]) & (atoms_gb['y'] == atom_pos[1]) & (
                             atoms_gb['z'] == atom_pos[2])], ignore_index=True)
             atoms_gb = df
-            atom_physics_features = atoms_gb[['atomic-charge', 'atomic-radius', 'effective-born-radii']].to_numpy()
+
+            # input features used by PGGCN
+            atom_physics_features = atoms_gb[['x', 'y', 'z', 'atomic-charge', 'atomic-radius', 'effective-born-radii']].to_numpy()
             atom_features = np.concatenate([mol_f.atom_features, atom_physics_features], axis=1)
+
+            # generate adjacency list with one hot vector (size 22) which indicates bond type
+            # this is a nested list ordered by atom id (idx in rdkit)
+            adjacency_list = []
+            for atom in mol.GetAtoms():
+                atom_bonds = []
+                for neighbor in atom.GetNeighbors():
+                    bond_type = mol.GetBondBetweenAtoms(atom.GetIdx(), neighbor.GetIdx()).GetBondType()
+                    neighbor_idx = neighbor.GetIdx()
+                    atom_bonds.append([neighbor_idx, self.convertBONDtoOneHotVector(bond_type)])
+                adjacency_list.append(atom_bonds)
+
+            inputs.append([atom_features, adjacency_list])
+
+        return inputs
+
+
+
+    @staticmethod
+    def convertBONDtoOneHotVector(bond_type):
+        BOND_DICT = {dir(Chem.rdchem.BondType)[i]: i for i in range(22)}
+        one_hot = np.zeros(22)
+        one_hot[BOND_DICT[str(bond_type)]] = 1
+        return one_hot
+
 
 
--- test.py
+++ test.py
@@ -1,46 +1,5 @@
-from deepchem.models.layers import GraphConv, GraphPool, GraphGather
-import tensorflow as tf
-import tensorflow.keras.layers as layers
-import deepchem as dc
-from deepchem.models.layers import GraphConv, GraphPool, GraphGather
-batch_size = 100
+from rdkit import Chem
 
-class MyGraphConvModel(tf.keras.Model):
+mol = Chem.MolFromPDBFile('Datasets/PDBBindSmallSet/1add-ligand.pdb')
 
-  def __init__(self):
-    super(MyGraphConvModel, self).__init__()
-    self.gc1 = GraphConv(128, activation_fn=tf.nn.relu)
-    self.batch_norm1 = layers.BatchNormalization()
-    self.gp1 = GraphPool()
 
-    self.gc2 = GraphConv(1, activation_fn=tf.nn.relu)
-    self.batch_norm2 = layers.BatchNormalization()
-    self.gp2 = GraphPool()
-
-    self.dense1 = layers.Dense(3, activation=tf.nn.relu)
-    self.batch_norm3 = layers.BatchNormalization()
-    self.readout = GraphGather(batch_size=batch_size, activation_fn=tf.nn.tanh)
-
-    self.dense2 = layers.Dense(1)
-    # self.logits = layers.Reshape((n_tasks, 2))
-    #add polar none polar here
-    self.dense3=layers.Dense(1)
-    self.ReLU = layers.ReLU()
-
-  def call(self, inputs):
-    x, g_pol, g_nonpol = inputs
-    gc1_output = self.gc1(x)
-    print(x)
-    batch_norm1_output = self.batch_norm1(gc1_output)
-    gp1_output = self.gp1([batch_norm1_output] + x[1:])
-    gc2_output = self.gc2([gp1_output] + x[1:])
-    batch_norm2_output = self.batch_norm1(gc2_output)
-    gp2_output = self.gp2([batch_norm2_output] + x[1:])
-
-    dense1_output = self.dense1(gp2_output)
-    batch_norm3_output = self.batch_norm3(dense1_output)
-    readout_output = self.readout([batch_norm3_output] + x[1:])
-
-    logits_output = self.logits(self.dense2(readout_output))
-    x = tf.concat([logits_output, g_pol, g_nonpol], axis=1)
-    return self.dense3(x)
