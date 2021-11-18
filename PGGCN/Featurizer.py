2c1fadb97422375d90151ec041ebe6316ed64084
ali <a_icam@yahoo.com>
2021-11-18 21:17:43 +0330

    PGGCN featurizer

--- PGGCN/Featurizer.py
+++ PGGCN/Featurizer.py
@@ -13,6 +13,14 @@ class PGGCNFeaturizer:
         self.atoms_csvs_directory = atom_csvs_directory if atom_csvs_directory[-1] == '/' else atom_csvs_directory + '/'
 
     def featurize(self):
+
+        # Find all atoms GBNSR6 features csv files
         atoms_csvs = [self.atoms_csvs_directory + atoms_csv for atoms_csv in os.listdir(self.atoms_csvs_directory) if atoms_csv.__contains__('.csv')]
+
+        # Create dictionary with key as PDB id and value as pandas Datafram
+        atoms_csvs_dict = {atoms_csv.split('/')[-1].split('.')[0].split('-')[0]: pd.read_csv(atoms_csv) for atoms_csv in
+                           atoms_csvs}
+
+
         for mol_name in self.mols_dict.keys():
 
