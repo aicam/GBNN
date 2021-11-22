58f7d0617226fd93a7b7e094bcfcfbc1a6828659
ali <a_icam@yahoo.com>
2021-11-22 17:50:58 +0330

    PGGCN layers started

--- PGGCN/layers.py
+++ PGGCN/layers.py
@@ -7,3 +7,13 @@ dc.models.GraphConvModel(n_tasks, mode='classification')
 
 dc.feat.ConvMolFeaturizer
 from deepchem.feat.mol_graphs import ConvMol
+
+class MultiGraphConvLayer(tf.keras.Model):
+    def __init__(self, operations = [[-1], [tf.math.reduce_sum]]):
+        super(MultiGraphConvLayer, self).__init__()
+
+    def call(self, inputs):
+        features = inputs[0]
+        adjacency_list = inputs[1]
+        physics_features = inputs[2]
+
