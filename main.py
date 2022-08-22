fix: ram overflow problem

main.py
@@ -1,11 +1,14 @@
 from GBModel.GB_Layers import MMLayer, GBLayer, FilterLayer
 import tensorflow as tf
 from deepchem.models.layers import GraphConv, GraphPool, GraphGather
-
+from keras.callbacks import EarlyStopping
 import numpy as np
 from deepchem.feat.mol_graphs import ConvMol
 x = np.ones((1, 8, 4, 3))
-
+import deepchem as dc
+dc.data.DiskDataset.from_numpy()
+from deepchem.feat import AtomicConvFeaturizer
+dc.models.KerasModel
 
 model = tf.keras.models.Sequential()
 model.add(MMLayer())