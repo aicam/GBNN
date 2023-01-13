0badc7e3f579ae2e74e119079e634300e376f3a1
ali <a_icam@yahoo.com>
2023-01-13 00:20:26 -0800

    fix: PGCN model

---
+++ PGGCN/layers_update.py
@@ -0,0 +1,195 @@
+import tensorflow as tf
+import numpy as np
+
+
+class RuleGraphConvLayer(tf.keras.layers.Layer):
+    def __init__(self,
+                 out_channel,
+                 num_features=81,
+                 num_bond=22,
+                 activation_fn=None,
+                 combination_rules=[]
+                 ):
+        super(RuleGraphConvLayer, self).__init__()
+        self.out_channel = out_channel
+        self.num_features = num_features
+        self.num_bond = num_bond
+        self.activation_fn = activation_fn
+        self.combination_rules = combination_rules
+        self.w_s = tf.Variable(initial_value=tf.initializers.glorot_uniform()
+        (shape=[num_features, out_channel]), shape=[num_features, out_channel], trainable=True)
+        self.w_n = tf.Variable(initial_value=tf.initializers.glorot_uniform()
+        (shape=[num_features + num_bond, out_channel]),
+                               shape=[num_features + num_bond, out_channel], trainable=True)
+        self.counter1 = tf.Variable(0, trainable=False)
+        self.self_conv_features = tf.Variable(
+            np.array([0.0 for _ in range(self.out_channel)]).reshape([1, self.out_channel]),
+            shape=[1, self.out_channel], trainable=False, dtype=tf.float32)
+
+    def AtomDistance(self, x, y):
+        return tf.sqrt(tf.reduce_sum(tf.square(x - y)))
+
+    def addRule(self, rule, start_index, end_index=None):
+        rules_dict = {
+            "sum": tf.math.add,
+            "multiply": tf.math.multiply,
+            "distance": "distance",
+            "divide": tf.math.divide,
+            "subtract": tf.math.subtract,
+        }
+        if type(rule) == str:
+            rule = rules_dict[rule]
+        if end_index == None:
+            self.combination_rules.append([[start_index], rule])
+        else:
+            self.combination_rules.append([[start_index, end_index], rule])
+
+    def _call_single(self, inp):
+        features = inp[0]
+        adjacency_list = inp[1]
+
+        # self.counter1.assign(0)
+        # self.self_conv_features.assign(np.array([0.0 for _ in range(self.out_channel)]).reshape([1, self.out_channel]))
+        def self_weight_mul():  # i, self_conv_features):
+            i = tf.constant(0)
+            self_conv_features = tf.TensorArray(size=self.out_channel, dynamic_size=True, dtype=tf.float32)
+            # condition
+            c = lambda i, self_conv_features: tf.less(i, features.shape[0])
+
+            # body
+            def b(i, self_conv_features):
+                self_conv_features = self_conv_features.write(i, tf.matmul(
+                    tf.reshape(features[i], [1, self.num_features]),
+                    self.w_s
+                ))
+                return i + 1, self_conv_features
+            return tf.while_loop(c, b, loop_vars=[i, self_conv_features])
+                                 #shape_invariants=[self.counter1.get_shape(), tf.TensorShape([None, self.out_channel])])
+        self_conv_features = self_weight_mul()[1]
+        # self.self_conv_features = self.self_conv_features[1:]
+        new_features = tf.reshape(self_conv_features.stack(), (features.get_shape()[0], self.out_channel))
+
+        ## Neighbours part
+        adj_dict = {}
+        adj_indices = [tf.constant(i).ref() for i in range(len(adjacency_list))]
+        for i, adj in enumerate(adjacency_list):
+            adj_dict[adj_indices[i]] = adj
+        def neighbour_weight_mul():
+            i = tf.constant(0)
+            nei_conv_features = tf.TensorArray(size=self.out_channel, dynamic_size=True, dtype=tf.float32)
+
+            c = lambda i, nei_conv_features: tf.less(i, features.shape[0])
+
+            def b(i, nei_conv_features):
+                new_ordered_features = tf.TensorArray(size=self.out_channel, dynamic_size=True, dtype=tf.float32, clear_after_read=False, infer_shape=False)
+                distance = -1.
+                adj_atom = tf.numpy_function(lambda x: adjacency_list[x], [i], tf.int32)
+                for neighbour in adj_atom:
+                    self_features = tf.reshape(features[i], [1, self.num_features])
+                    for j, rule in enumerate(self.combination_rules):
+                        if j == len(self.combination_rules) - 1 and len(rule[0]) == 1:
+                            new_ordered_features.write(j, rule[1](self_features[0][rule[0][0]:],
+                                                                  features[neighbour[0]][rule[0][0]:]))
+                        else:
+                            if rule[1] == 'distance':
+                                distance = self.AtomDistance(x=self_features[0][rule[0][0]:rule[0][1]],
+                                                             y=features[neighbour[0]][rule[0][0]:rule[0][1]])
+                                new_ordered_features.write(j, features[neighbour[0]][rule[0][0]:rule[0][1]])
+                            else:
+                                new_ordered_features.write(j, rule[1](self_features[0][rule[0][0]:rule[0][1]],
+                                                                      features[neighbour[0]][rule[0][0]:rule[0][1]]))
+                    new_ordered_features_tensor = tf.concat([new_ordered_features.read(k) for k in range(len(self.combination_rules))], axis=0)
+                    if distance != -1.:
+                        distance = distance if distance > 0 else 10e-3
+                        new_ordered_features_tensor /= distance ** 2
+                    nei_conv_features.write(i,
+                        tf.add(
+                            tf.matmul(
+                                tf.reshape(
+                                    tf.concat(
+                                        [new_ordered_features_tensor, tf.cast(neighbour[1], dtype=tf.float32)],
+                                    axis=0),
+                                [1, self.num_features + self.num_bond]),
+                            self.w_n),
+                        new_features[i])
+                    )
+
+                return i + 1, nei_conv_features
+            return tf.while_loop(c, b, loop_vars=[i, nei_conv_features])
+
+        neighbor_conv_features = neighbour_weight_mul()[1]
+
+        # self.self_conv_features = self.self_conv_features[1:]
+        neighbor_conv_features = tf.reshape(neighbor_conv_features.stack(), (features.get_shape()[0], self.out_channel))
+        return neighbor_conv_features
+
+    def call(self, inputs):
+        output = []
+        for inp in inputs:
+            ans = self._call_single(inp)
+            output.append(ans)
+        return output
+
+
+class ConvLayer(tf.keras.Model):
+    def __init__(self, out_channel, num_features=40):
+        super(ConvLayer, self).__init__()
+        self.out_channel = out_channel
+        self.num_features = num_features
+        self.w = tf.Variable(tf.initializers.glorot_uniform()
+                             (shape=[num_features, out_channel]), shape=[num_features, out_channel], trainable=True)
+
+    def _call_single(self, inp):
+        out = tf.zeros(shape=[1, self.out_channel], dtype=tf.float32)
+        for feature in inp:
+            feature = tf.reshape(feature, [1, -1])
+            feature = tf.cast(feature, 'float')
+            out += tf.reshape(tf.nn.softmax(tf.matmul(feature, self.w)), [-1])
+        return tf.reshape(out, [-1])
+
+    def call(self, inputs):
+        output = []
+        for inp in inputs:
+            output.append(self._call_single(inp))
+        return tf.reshape(output, [len(inputs), -1])  # np.array(output).reshape([len(inputs), -1])
+
+
+class GraphConvLayer(tf.keras.layers.Layer):
+    def __init__(self,
+                 out_channel,
+                 num_features=80,
+                 activation_fn=tf.keras.activations.relu,
+                 ):
+        super(GraphConvLayer, self).__init__()
+        self.out_channel = out_channel
+        self.num_features = num_features
+        self.activation_fn = activation_fn
+        self.w_s = tf.Variable(initial_value=tf.initializers.glorot_uniform()
+        (shape=[num_features, out_channel]), shape=[num_features, out_channel], trainable=True)
+        self.w_n = tf.Variable(initial_value=tf.initializers.glorot_uniform()
+        (shape=[num_features, out_channel]),
+                               shape=[num_features, out_channel], trainable=True)
+
+    def _call_single(self, inp):
+        features = inp[0]
+        adjacency_list = inp[1]
+        new_features = features.shape[0] * [None]
+        for i, adj in enumerate(adjacency_list):
+            self_features = tf.reshape(features[i], [1, self.num_features])
+            self_conv_features = tf.matmul(self_features, self.w_s)
+            new_features[i] = self_conv_features
+            for neighbour in adj:
+                neighbour_features = features[neighbour[0]]
+                neighbour_bond = neighbour[1]
+                neighbour_features = tf.reshape(neighbour_features, [1, self.num_features])
+                new_features[i] += tf.matmul(neighbour_features, self.w_n)
+                new_features[i] = new_features[i][0]
+                if self.activation_fn != None:
+                    new_features[i] = self.activation_fn(new_features[i])
+        return ([tf.Variable(new_features, trainable=False), adjacency_list])
+
+    def call(self, inputs):
+        output = []
+        for inp in inputs:
+            output.append(self._call_single(inp))
+        return output
---
+++ PGGCN/runner.py
@@ -0,0 +1,169 @@
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+# !curl -Lo conda_installer.py https://raw.githubusercontent.com/deepchem/deepchem/master/scripts/colab_install.py
+import conda_installer
+
+# conda_installer.install()
+conda_installer.install()
+# !/root/miniconda/bin/conda info -e
+import rdkit
+
+from deepchem.feat.graph_features import atom_features as get_atom_features
+
+df = pd.read_csv('Datasets/Mobley/info.csv')
+df = df.dropna()
+df = df[df['Dataset Name'] == 'cd-set1']
+
+training_cols = [col for col in df.columns if (col[:3] == 'gb_' and not col.__contains__('Etot') and not col.__contains__('Ex_') and not col.__contains__('delta')) or (col.__contains__('VDWAALS'))]
+# training_cols = ['gb_Ex_difference']
+
+PDBs = {}
+from os import listdir
+from os.path import isfile, join
+mypath = 'Datasets/Mobley/cd-set1/pdb/'
+onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+host_name = [x for x in onlyfiles if x.__contains__('host')][0]
+host_mol = rdkit.Chem.rdmolfiles.MolFromPDBFile(mypath + '/' + host_name)
+onlyfiles.remove(host_name)
+for f in onlyfiles[:3]:
+    guest_mol = rdkit.Chem.rdmolfiles.MolFromPDBFile(mypath + '/' + f)
+    PDBs.update({f.split('.')[0] : rdkit.Chem.CombineMols(host_mol,guest_mol)})
+
+
+def featurize(molecule):
+    def convertBONDtoOneHotVector(bond_type):
+        BOND_DICT = {dir(rdkit.Chem.rdchem.BondType)[i]: i for i in range(22)}
+        one_hot = np.zeros(22)
+        one_hot[BOND_DICT[str(bond_type)]] = 1
+        return one_hot
+
+    atom_features = []
+    adjacency_list = []
+    for atom in molecule.GetAtoms():
+        new_feature = get_atom_features(atom).tolist()
+        position = molecule.GetConformer().GetAtomPosition(atom.GetIdx())
+        new_feature += [atom.GetMass(), atom.GetAtomicNum()]
+        new_feature += [position.x, position.y, position.z]
+        atom_features.append(new_feature)
+    atom_features = np.array(atom_features)
+    for atom in molecule.GetAtoms():
+        atom_bonds = []
+        for neighbor in atom.GetNeighbors():
+            bond_type = molecule.GetBondBetweenAtoms(atom.GetIdx(), neighbor.GetIdx()).GetBondType()
+            neighbor_idx = neighbor.GetIdx()
+            atom_bonds.append([neighbor_idx, convertBONDtoOneHotVector(bond_type)])
+        adjacency_list.append(atom_bonds)
+    return [atom_features, adjacency_list]
+
+X = []
+y = []
+info = []
+for pdb in list(PDBs.keys()):
+    X.append(featurize(PDBs[pdb]))
+    info.append(df[df['Guest'] == pdb.replace('-s', '-')][training_cols].to_numpy()[0])
+    y.append(df[df['Guest'] == pdb.replace('-s', '-')]['EX _H_(kcal/mol)'].abs().to_numpy()[0])
+info = np.array(info)
+
+import layers
+import importlib
+
+
+class PGGCNModel(tf.keras.Model):
+    def __init__(self, num_atom_features=80, r_out_channel=40, c_out_channel=1024):
+        super().__init__()
+        self.ruleGraphConvLayer = layers.RuleGraphConvLayer(r_out_channel, num_atom_features)
+        self.ruleGraphConvLayer.combination_rules = []
+        self.conv = layers.ConvLayer(c_out_channel)
+        self.dense1 = tf.keras.layers.Dense(300, activation='sigmoid')
+        self.dense2 = tf.keras.layers.Dense(450, activation='sigmoid')
+        self.dense3 = tf.keras.layers.Dense(250, activation='sigmoid')
+        # self.dense4 = tf.keras.layers.Dense(150, activation='relu')
+        # self.dense5 = tf.keras.layers.Dense(50, activation='relu')
+        self.dense6 = tf.keras.layers.Dense(1)
+        # self.dense7 = tf.keras.layers.Dense(1,
+        #                                     kernel_initializer=tf.keras.initializers.Constant(
+        #                                         [.5, -1, -1, 1, 1, 1, 1, 1, -1, -1, -1, -1, -1, -1, -1, -1]),
+        #                                     bias_initializer=tf.keras.initializers.Zeros(),
+        #                                     activation=tf.keras.activations.relu)
+
+    def set_physics_info(self, info):
+        self.physics_info = info.reshape([-1, 15])
+
+    def addRule(self, rule, start_index, end_index=None):
+        self.ruleGraphConvLayer.addRule(rule, start_index, end_index)
+
+    def set_input(self, all_inputs):
+        self.all_inputs = all_inputs
+
+    def set_adjacency_list(self, a_l):
+        self.a_l = a_l
+
+    def set_input_shapes(self, i_s):
+        self.i_s = i_s
+
+    def call(self, inputs):
+        x_a = []
+        for i in range(len(self.i_s)):
+            x_a.append(inputs[i][:self.i_s[i]])
+        #         agg = [[x_a, self.a_l]]
+        agg = []
+        for i in range(len(x_a)):
+            agg.append([x_a[i], self.a_l[i]])
+        #         agg = self.all_inputs
+        #         inputs = self.all_inputs
+        x = self.ruleGraphConvLayer(agg)
+        x = self.conv(x)
+        x = self.dense1(x)
+        x = self.dense2(x)
+        x = self.dense3(x)
+        # x = self.dense4(x)
+        # x = self.dense5(x)
+        model_var = self.dense6(x)
+        # merged = tf.concat([model_var, self.physics_info], axis=1)
+        # out = self.dense7(merged)
+        return model_var
+
+
+m = PGGCNModel()
+m.addRule("sum", 0, 75)
+m.addRule("multiply", 75, 77)
+m.addRule("distance", 77, 80)
+m.set_physics_info(info)
+
+import keras.backend as K
+
+
+def root_mean_squared_error(y_true, y_pred):
+    return K.sqrt(K.mean(K.square(y_pred[0] - y_true))) + (1 / K.mean(.5 + y_pred[1]))
+
+
+def pure_rmse(y_true, y_pred):
+    return K.sqrt(K.mean(K.square(y_pred[0] - y_true)))
+
+
+m.compile(loss=pure_rmse, optimizer='adam')
+X_atoms = []
+X_adj = []
+for x in X:
+    X_atoms.append(x[0])
+    X_adj.append(x[1])
+input_shapes = []
+for i in range(len(X_atoms)):
+    input_shapes.append(np.array(X_atoms[i]).shape[0])
+m.set_input_shapes(input_shapes)
+for i in range(len(X_atoms)):
+    if X_atoms[i].shape[0] < 77:
+        new_list = X_atoms[i].tolist()
+        for j in range(80 - X_atoms[i].shape[0]):
+            new_list.append([0.0] * 80)
+        #         new_list.append(np.concatenate((info[i], [0]*65)))
+        X_atoms[i] = np.array(new_list)
+m.set_adjacency_list(X_adj)
+m.set_physics_info(info)
+X_atoms = np.array(X_atoms)
+y = np.array(y)
+hist = m.fit(X_atoms, y, epochs=20)
+import pickle
+with open('res.pkl', 'wb') as file:
+    pickle.dump(hist.history, file)
