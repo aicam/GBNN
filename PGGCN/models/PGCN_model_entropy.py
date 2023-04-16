307944dd25af4ada4a26ea1c64719535499f57c0
ali <a_icam@yahoo.com>
2023-04-15 21:45:40 -0700

    feat: result for JCIM

--- PGGCN/models/PGCN_model_entropy.py
+++ PGGCN/models/PGCN_model_entropy.py
@@ -16,10 +16,10 @@ class PGGCNModel(tf.keras.Model):
         self.ruleGraphConvLayer.combination_rules = []
         self.conv = layers.ConvLayer(c_out_channel)
         self.dense1 = tf.keras.layers.Dense(64, activation='relu', name='dense1')
-        self.dense5 = tf.keras.layers.Dense(16, name='relu')
+        self.dense5 = tf.keras.layers.Dense(32, name='relu')
         self.dense6 = tf.keras.layers.Dense(1, name='dense6')
         self.dense7 = tf.keras.layers.Dense(1, name='dense7',
-                                            kernel_initializer=tf.keras.initializers.Constant([.3, -1, 1, 1]),
+                                            kernel_initializer=tf.keras.initializers.Constant([-.3, -1, 1, 1]),
                                             bias_initializer=tf.keras.initializers.Zeros())
 
     def addRule(self, rule, start_index, end_index=None):
